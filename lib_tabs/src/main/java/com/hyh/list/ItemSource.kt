ff175efbddd367b3ac8238805096c683a0f57746
EricHyh <252882325@qq.com>
2021-06-24 00:00:57 +0800

    ...

--- lib_tabs/src/main/java/com/hyh/list/ItemSource.kt
+++ lib_tabs/src/main/java/com/hyh/list/ItemSource.kt
@@ -8,10 +8,12 @@ import kotlinx.coroutines.Dispatchers
 abstract class ItemSource<Param : Any> {
 
     companion object {
+        const val DEFAULT_ITEMS_BUCKET_ID = -1
         val NONE_TOKEN = Any()
     }
 
     internal val delegate: Delegate<Param> = object : Delegate<Param>() {
+
         override fun activate() {
         }
 
@@ -42,6 +44,7 @@ abstract class ItemSource<Param : Any> {
     val refreshActuator: RefreshActuator
         get() = _refreshActuator
 
+    private var itemsBucketIds: List<Int> = listOf(DEFAULT_ITEMS_BUCKET_ID)
 
     open fun onUpdateItemSource(oldPosition: Int, newPosition: Int, newItemSource: ItemSource<Param>) {}
 
@@ -53,12 +56,23 @@ abstract class ItemSource<Param : Any> {
     open suspend fun onLoadResult(params: LoadParams<Param>, loadResult: LoadResult) {}
     open fun getFetchDispatcher(param: Param): CoroutineDispatcher = Dispatchers.Unconfined
 
+
+    protected fun registerItemsBucketIds(itemsBucketIds: List<Int>) {
+
+    }
+
+    protected fun markItems(items: List<ItemData>, itemsToken: Any) {
+
+    }
+
     abstract class Delegate<Param : Any> {
+
         abstract fun activate()
         abstract fun initPosition(position: Int)
         abstract fun injectRefreshActuator(refreshActuator: RefreshActuator)
         abstract fun updateItemSource(newPosition: Int, newItemSource: ItemSource<Param>)
-        abstract fun onItemsTokenChanged(
+        abstract fun onBucketItemsTokenChanged(
+            bucketId: Int,
             oldItemsToken: Any,
             oldItems: List<ItemData>,
             newItemsToken: Any,
@@ -88,7 +102,8 @@ abstract class ItemSource<Param : Any> {
 
         data class Success constructor(
             val items: List<ItemData>,
-            val itemsToken: Any = NONE_TOKEN
+            val itemsBucketIds: List<Int>,
+            val itemsBuckets: List<ItemsBucket>
         ) : PreShowResult()
     }
 
@@ -100,9 +115,16 @@ abstract class ItemSource<Param : Any> {
 
         data class Success constructor(
             val items: List<ItemData>,
-            val itemsToken: Any = NONE_TOKEN
+            val itemsBucketIds: List<Int>,
+            val itemsBuckets: List<ItemsBucket>
         ) : LoadResult()
     }
+
+    data class ItemsBucket(
+        val bucketId: Int,
+        val itemsToken: Any,
+        val items: List<ItemData>,
+    )
 
     class PreShowParams<Param : Any>(
         val param: Param,
